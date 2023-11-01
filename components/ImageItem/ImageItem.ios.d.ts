5e5f2d64fc81133d181d3976561f80f85c088e92
Yakhatin <sasha.yahatin@yandex.ru>
2023-11-01 18:58:32 +0300

    build

---
+++ components/ImageItem/ImageItem.ios.d.ts
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) JOB TODAY S.A. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+import React, { ComponentType } from "react";
+import { ImageSource } from "../../@types";
+declare type Props = {
+    imageSrc: ImageSource;
+    onRequestClose: () => void;
+    onZoom: (scaled: boolean) => void;
+    onLongPress: (image: ImageSource) => void;
+    delayLongPress: number;
+    swipeToCloseEnabled?: boolean;
+    doubleTapToZoomEnabled?: boolean;
+    loader?: ComponentType;
+};
+declare const _default: React.MemoExoticComponent<({ imageSrc, onZoom, onRequestClose, onLongPress, delayLongPress, swipeToCloseEnabled, doubleTapToZoomEnabled, loader, }: Props) => JSX.Element>;
+export default _default;
